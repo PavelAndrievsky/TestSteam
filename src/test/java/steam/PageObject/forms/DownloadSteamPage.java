0a3521b177a94717213ed88aba22278fa175e5e5
Pasha7 <barsadetka@gmail.com>
2018-07-24 12:33:15 +0300

    add

--- src/test/java/steam/PageObject/forms/DownloadSteamPage.java
+++ src/test/java/steam/PageObject/forms/DownloadSteamPage.java
@@ -1,6 +1,7 @@
 package steam.PageObject.forms;
 
 import framework.BasePage;
+import framework.Browser;
 import framework.CommonFunctions;
 import framework.elements.Button;
 import framework.HttpUtils;
@@ -12,17 +13,17 @@ public class DownloadSteamPage extends BasePage {
 
     private static final String downloadFile = "src/test/resources/SteamSetup.exe";
 
-    Button btnDownload;
+    private Button btnDownload;
     public DownloadSteamPage() {}
 
     public boolean checkFullDownload() {
         btnDownload = new Button(By.xpath((locator)));
         String link =  btnDownload.getAttribute("href");
-        System.out.println(link);
         String filePath = downloadFile;
         CommonFunctions commonFunctions = new CommonFunctions();
         HttpUtils utils = new HttpUtils();
         long size = utils.getSizeOfContent(link, filePath);
+        Browser.waitPageLoad();
         return commonFunctions.checkFullDownLoad(filePath, size);
     }
 
